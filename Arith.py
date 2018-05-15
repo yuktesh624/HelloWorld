cfe963d3af7d7cdb358672c8fa1c06a72b76a4eb
yuktesh624 <38282531+yuktesh624@users.noreply.github.com>
2018-05-15 16:18:27 +0530

    Add files via upload

---
+++ Arith.py
@@ -0,0 +1,17 @@
+#Arithmetic Operators
+
+x=15
+y=6
+print("x+y = ",x+y)
+
+print("x-y = ",x-y)
+
+print("x*y = ",x*y)
+
+print("x/y = ",x/y)
+
+print("x%y = ",x%y)
+
+print("x//y = ",x//y)
+
+print("x**y = ",x**y)
